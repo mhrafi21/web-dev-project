2ed9ec24ebb25a76dc8c42f669f06b5122545a89
12mhrafi <12mhrafi>
2023-07-22 22:16:53 +0600

    updated

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const navbar = document.getElementById("nav-bar")
 toggle.addEventListener("click", ()=>{
     navbar.classList.toggle("active");
 })
+
+// counter
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("#header");
+   
+});
+
+
+
+  window.addEventListener("scroll", () => {
+    const innerH = scrollY;
+    console.log(innerH);
+    if (innerH >= 1600) {
+      const counters = document.querySelectorAll(".counter");
+      const speed = 1000;
+      counters.forEach((counter) => {
+        const updateCounter = () => {
+          const target = parseInt(counter.getAttribute("data-target"));
+          const count = parseInt(counter.innerText);
+          const inc = Math.floor(target / speed);
+          if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCounter, 1000);
+          } else {
+            count.innerText = target;
+          }
+        };
+        updateCounter();
+      });
+    }
+  
+  });
